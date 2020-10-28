f12051166fc2fae3134b3353193777a73cb7aa92
Derek Li <derek.j.h.li@gmail.com>
2020-10-28 22:35:57 +0800

    second commit

--- products.py
+++ products.py
@@ -1,11 +1,8 @@
 products = []
 while True:
-	product = []
 	name = input("請輸入商品名稱：　")
 	if name == "q":
 		break
 	price = input("請輸入商品價格：　")
-	product.append(name)
-	product.append(price)
-	products.append(product)
+	products.append([name, price])
 print(products)
