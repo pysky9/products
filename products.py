17ba168d7b95870136f04fa8e20ee95d62127def
Derek Li <derek.j.h.li@gmail.com>
2020-11-01 12:05:41 +0800

    refactor

--- products.py
+++ products.py
@@ -1,32 +1,51 @@
-import os
 # 讀取檔案
-products = []
-if os.path.isfile("products.csv"): #檢查檔案是否存在
-	print("Yeah! Find the file")
-	with open("products.csv", "r", encoding = "utf-8") as f:
-		for line in f:
-			if "商品,價格" in line:
+def read_file(filename, products):
+	with open(filename, "r", encoding = "utf-8") as f:
+		for p in f:
+			if "商品,價格" in p:
 				continue
-			name, price = line.strip().split(",")
+			name, price = p.strip().split(",")
 			products.append([name, price])
 	print(products)
-else:
-	print("oh....cannot find the file")
+	return products
 
-# 讓使用者輸入購買紀錄
-while True:
-	name = input("請輸入商品名稱：　")
-	if name == "q":
-		break
-	price = input("請輸入商品價格：　")
-	products.append([name, price])
+#使用者記帳
+def buying_record(products):
+	while True :
+		name = input("請輸入商品名稱：")
+		price = int(input("請輸入商品價格："))
+		products.append([name, price])
+		quit = input("請問還需繼續記帳嗎(Y/N):")
+		if quit.lower() == "n":
+			break
+	print(products)
+	return products
 
-# 印出所有購買紀錄
-for p in products:
-	print(f"{p[0]}的價格為{p[1]}元")
+# 列印記帳明細
+def buying_detail(products):
+	for line in products:
+		print(f"{line[0]}的價格為{line[1]}元")
+	return line
 
-# 寫入檔案
-with open("products.csv", 'w', encoding = 'utf-8') as f:
-	f.write("商品,價格\n")
-	for p in products:
-		f.write(p[0] + "," + p[1] + "\n") # CSV檔以逗點(,)區隔
+# 寫成CSV檔 write_file
+def write_file(filename, products):
+	with open(filename, "w", encoding = "utf-8") as f:
+		f.write("商品,價格\n")
+		for line in products:
+			f.write(line[0] + "," + str(line[1]) + "\n")
+
+# 程式啟動點
+def main():
+	products = []
+	import os
+	filename = "products.csv"
+	if os.path.isfile(filename):
+		print("file was found")
+		read_file(filename, products)
+	else:
+		print("file doesn't be found")
+	buying_record(products)
+	buying_detail(products)
+	write_file(filename, products)
+	return products
+main()
